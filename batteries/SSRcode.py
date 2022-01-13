99834bf34b2e0a84bed1365225eba6f3d4ecf6ec
mglasser16 <mglasser@mines.edu>
2022-01-13 12:06:09 -0700

    Functional SSR code

--- batteries/SSRcode.py
+++ batteries/SSRcode.py
@@ -4,32 +4,58 @@ import pandas as pd
 import os
 
 
-path = 'C:\Users\Mels\Code\Data\ToProcess' #change this to file directory
-modelfile = 'file'
-comparison = 'file'
-#%%
-capacity1 =5
-capacity2 = 4
-voltage1 = 2.92
-voltage2 =2.93
-capacity3 = 4.5
+path = 'D:\projects\Data\ToProcess' #change this to file directory
+modelfile = 'Refdata1.csv'
+comparison = 'Refdata.xlsx'
 
+# CPCN04 = "50CP50CNT0.4"
+# CP04 = "CP0.4"
+# 50CP50CNT0.5
+# CP0.5
+# 50CP50CNT0.6
+# CP0.6
+#%%
+
+Capacity_test = [0.1,0.2, 0.3]
+Voltage_test = [0, 0, 0]
 
 
 def interpolate(voltage1, voltage2, capacity1, capacity2, capacity3):
     voltage3 =  (voltage2 - voltage1)*(capacity3-capacity1)/(capacity2-capacity1) +voltage1
     return voltage3
 
-def graph(file):
-    Data = pd.read_table(path + "/"+ modelfile, sep=',', engine='python')
-    Data2 = pd.read_table(path + "/"+ comparison, sep=',', engine='python')
-    for row in Data['Capacity']:
-        print (Data['Capacity'][row])
-    # lowerneighbour_ind = Data2[Data2['Capacity'] < Temp]['Capacity'].idxmax()
-    # higherneighbour_ind = Data2[Data2['Capacity'] > Temp]['Capacity'].idxmin()
-    # v3 = interpolate(Data2['Capacity'][higherneighbour_ind], voltage2,Data2['Capacity'][lowerneighbour_ind], Data2['Capacity'][higherneighbour_ind], capacity3)
+def locater(dataset, i, refcapacity):
+    lowerneighbour_ind = RefData[dataset][Data[dataset]['Capacity'] < CodeData['Capacity'][i]]['Capacity'].idxmax()
+    higherneighbour_ind = RefData[dataset][Data[dataset]['Capacity'] > CodeData['Capacity'][i]]['Capacity'].idxmin()
+    capacity1 = RefData[dataset]['Capacity'][lowerneighbour_ind]
+    capacity2 = RefData[dataset]['Capacity'][higherneighbour_ind]
+    capacity3 = refcapacity
+    voltage1 = RefData[dataset]['Voltage'][lowerneighbour_ind]
+    voltage2 = RefData[dataset]['Voltage'][higherneighbour_ind]
+    v3 = (interpolate(voltage1, voltage2, capacity1, capacity2, capacity3))
+    print(i)
+    SSR = (CodeData['Capacity'][i] - v3)**2
+    return SSR
 
+
+RefData = pd.read_excel(path + "/"+ comparison, sheet_name= None)
+CodeData = pd.read_csv(path + "/"+ modelfile)
+
+for x, y in enumerate(CodeData['Capacity']):
+    SumSR += locater("50CP50CNT0.4", x, y)
+    print(SumSR)
+
+#result = [locater("50CP50CNT0.4", x, refcapcity) for x, refcapcity in enumerate(CodeData['Capacity'])]
+#print(result)
+
+# print (Data['50CP50CNT0.4']['Capacity'][row])
+# lowerneighbour_ind = Data2[Data2['Capacity'] < Temp]['Capacity'].idxmax()
+# higherneighbour_ind = Data2[Data2['Capacity'] > Temp]['Capacity'].idxmin()
+# v3 = interpolate(Data2['Capacity'][higherneighbour_ind], voltage2,Data2['Capacity'][lowerneighbour_ind], Data2['Capacity'][higherneighbour_ind], capacity3)
+# SSR += (Voltage_test[i] - v3)**2
 
 
 
 # %%
+# Official Playlist
+# Small World - Bump of Chicken
