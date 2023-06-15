3d3fb1c08906e530c3fdb31dd59c6eb531f7c7d0
mglasser16 <mglasser@mines.edu>
2023-06-15 09:10:10 -0600

    Graphs for batcan paper

--- Batcan_post_process/heatmap.py
+++ Batcan_post_process/heatmap.py
@@ -10,10 +10,12 @@ plt.rcParams.update({'font.size': 18})
 
 
 #%%
-path = 'D:/projects/Data/Batcan/summary.csv'
+path = 'D:/projects/Data/Batcan/summary_smax.csv'
 bulkdata = pd.read_csv(path, header = 0)
 bulkdata.columns
-bulkdata['density'] = 1.0869 +934.1*bulkdata["Thickness"]*(1-bulkdata["Porosity"])+2216**bulkdata["Thickness"]*(bulkdata["Porosity"])
+# bulkdata['density'] = 1.0869 +934.1*bulkdata["Thickness"]*(1-bulkdata["Porosity"])+2216**bulkdata["Thickness"]*(bulkdata["Porosity"])
+bulkdata['density'] = 1.0869 +934.1*bulkdata["Thickness"]*(1-0.5)+2216**bulkdata["Thickness"]*(0.5)
+
 bulkdata['GHG Emissions [kg CO2 m-2]'] = 65.77324472 + 27192.25698*bulkdata["Thickness"]*(1-bulkdata["Porosity"])+149407.4639*bulkdata["Thickness"]*(bulkdata["Porosity"])
 bulkdata["Thickness"] =bulkdata["Thickness"]*1000000 #convert from m
 bulkdata['En_Den'] = bulkdata['Energy Density']/bulkdata['density']
@@ -21,8 +23,8 @@ bulkdata['GHG Emissions [kg CO2 kWh-2]'] = bulkdata['GHG Emissions [kg CO2 m-2]'
 bulkdata['Pow Den'] = bulkdata['Power Density']/bulkdata['density']
 # ed_max = bulkdata['En Den'].max() #Wh/m2
 # ed_min = bulkdata['En Den'].min()
-# pd_max = bulkdata['Pow Den'].max() #W/m2
-# pd_min = bulkdata['Pow Den'].min()
+# d_max = bulkdata['Delta'].max() #W/m2
+# d_min = bulkdata['Delta'].min()
 
 bulkdata.loc[bulkdata['Current'] == '0p5_liu', 'Current'] = '0.5 mA cm⁻²'
 bulkdata.loc[bulkdata['Current'] == '0p2_liu', 'Current'] = '0.2 mA cm⁻²'
@@ -38,7 +40,11 @@ array.sort()
 #change labels
 #Share-x and share-y
 #Change current
-pointer = np.arange(0.20,0.84,0.05)[::-1].round(2)
+
+#%%
+#For Four current plots
+
+pointer = np.arange(0.35,0.65,0.05)[::-1].round(2)
 fig, axes = plt.subplots(2, 2, figsize=(20,14))
 plt.subplots_adjust(wspace=0, hspace=0.1)
 fig2, axes2 = plt.subplots(2, 2, figsize=(20,14))
@@ -48,7 +54,7 @@ plt.subplots_adjust(wspace=0, hspace=0.1)
 # fig4, axes4 = plt.subplots(2, 2, figsize=(20,14))
 # plt.subplots_adjust(wspace=0, hspace=0.1)
 
-plt.plot(bulkdata['En_Den'], bulkdata["GHG Emissions [kg CO2 kWh-2]"], marker ='o', linestyle = 'none')
+# plt.plot(bulkdata['En_Den'], bulkdata["GHG Emissions [kg CO2 kWh-2]"], marker ='o', linestyle = 'none')
 
 
 
@@ -80,12 +86,125 @@ for n, current in enumerate(array):
     print(Point)
     energy_density = data.pivot('Porosity [-]','Thickness (μm)', 'En_Den')
     power_density = data.pivot('Porosity [-]', 'Thickness (μm)', "Pow Den")
+    # Delta = data.pivot('Porosity [-]', 'Thickness (μm)', "Delta")
     # capacity = data.pivot('Porosity [-]', 'Thickness (μm)', "Capacity")
     GHG= data.pivot('Porosity [-]', 'Thickness (μm)', "GHG Emissions [kg CO2 kWh-2]")
     sns.heatmap(energy_density, ax=axes[a,b]).set_title(current + ': Max Energy Density =' + str(round(ed_max)) + ' Wh kg⁻¹') #, vmin=ed_min, vmax=ed_max
     sns.heatmap(power_density, ax=axes2[a,b]).set_title(current + ': Max Power Density =' + str(round(pd_max,1)) + ' W kg⁻¹') #, vmin=pd_min, vmax=pd_max
-    # sns.heatmap(capacity, ax=axes4[a,b]).set_title(current + ': Max Capacity =' + str(round(c_max,1)) + ' mAh g⁻¹') #, vmin=pd_min, vmax=pd_max
+    # sns.heatmap(Delta, ax=axes4[a,b], cmap="seismic").set_title(current ) #, vmin=-1*d_max, vmax=d_max
     sns.heatmap(GHG, ax=axes3[a,b], cmap="crest").set_title(current + ': Min GHG =' + str(round(g_min)) + ' kg CO2 kWh⁻¹') #, vmin=pd_min, vmax=pd_max
+
+axes[0,0].set(xticklabels=[])
+axes[0,1].set(xticklabels=[])
+# axes[0,0].set(yticklabels=pointer)
+# axes[1,0].set(yticklabels=pointer)
+axes[0,0].set(xlabel=None)
+axes[0,1].set(xlabel=None)
+axes[0,0].tick_params(bottom=False)
+axes[0,1].tick_params(bottom=False)
+
+axes[1,1].set(yticklabels=[])
+axes[0,1].set(yticklabels=[])
+# axes2[0,0].set(yticklabels=pointer)
+# axes2[1,0].set(yticklabels=pointer)
+axes[0,1].tick_params(left=False)
+axes[1,1].tick_params(left=False)
+axes[0,1].set(ylabel=None)
+axes[1,1].set(ylabel=None)
+
+# fig.savefig('heatmap_ed'+ '.png', format='png', dpi=900, bbox_inches = "tight")
+fig.savefig('graded_pc'+ '.png', format='png', dpi=300, bbox_inches = "tight")
+axes2[0,0].set(xticklabels=[])
+axes2[0,1].set(xticklabels=[])
+# axes2[0,0].set(yticklabels=pointer)
+# axes2[1,0].set(yticklabels=pointer)
+axes2[0,0].set(xlabel=None)
+axes2[0,1].set(xlabel=None)
+axes2[0,0].tick_params(bottom=False)
+axes2[0,1].tick_params(bottom=False)
+
+axes2[1,1].set(yticklabels=[])
+axes2[0,1].set(yticklabels=[])
+axes2[0,1].tick_params(left=False)
+axes2[1,1].tick_params(left=False)
+axes2[0,1].set(ylabel=None)
+axes2[1,1].set(ylabel=None)
+
+# fig2.savefig('heatmap_pd'+ '.png', format='png', dpi=900, bbox_inches = "tight")
+
+axes3[0,0].set(xticklabels=[])
+axes3[0,1].set(xticklabels=[])
+axes3[0,0].set(yticklabels=pointer)
+axes3[1,0].set(yticklabels=pointer)
+axes3[0,0].set(xlabel=None)
+axes3[0,1].set(xlabel=None)
+axes3[0,0].tick_params(bottom=False)
+axes3[0,1].tick_params(bottom=False)
+
+axes3[1,1].set(yticklabels=[])
+axes3[0,1].set(yticklabels=[])
+axes3[0,1].tick_params(left=False)
+axes3[1,1].tick_params(left=False)
+axes3[0,1].set(ylabel=None)
+axes3[1,1].set(ylabel=None)
+
+
+#%%
+## For two current plots
+
+pointer = np.arange(0.35,0.65,0.05)[::-1].round(2)
+fig, axes = plt.subplots(2, 2, figsize=(20,14))
+plt.subplots_adjust(wspace=0, hspace=0.1)
+fig2, axes2 = plt.subplots(2, 2, figsize=(20,14))
+plt.subplots_adjust(wspace=0, hspace=0.1)
+fig3, axes3 = plt.subplots(2, 2, figsize=(20,14))
+plt.subplots_adjust(wspace=0, hspace=0.1)
+
+# plt.plot(bulkdata['En_Den'], bulkdata["GHG Emissions [kg CO2 kWh-2]"], marker ='o', linestyle = 'none')
+
+
+
+for n, current in enumerate(array):
+    if n<2:
+        a = 0
+    else:
+        a = 1
+    if n % 2 == 0:
+        b=0 # Even
+    else:
+        b=1 # Odd
+    data = bulkdata[bulkdata.Current == current]
+    # plt.figure()
+    # plt.plot(data['En_Den'], data["GHG Emissions [kg CO2 kWh-2]"], marker ='o', linestyle = 'none')
+    # plt.ylabel('Energy Density [Wh kg⁻¹]')
+    # plt.xlabel('GHG Emissions [ kg CO2 kWh⁻¹]')
+    # plt.savefig(str(current)+'.png', format='png', dpi=900, bbox_inches = "tight")
+    # plt.yscale('log')
+    ed_max = data['En_Den'].max() #Wh/m2
+    Point = bulkdata[bulkdata.En_Den == ed_max]
+
+    ed_min = data['En_Den'].min()
+    pd_max = data['Pow Den'].max() #W/m2
+    pd_min = data['Pow Den'].min()
+    g_min = data["GHG Emissions [kg CO2 kWh-2]"].min()
+    d2_max = data['Delta'].min() #W/m2
+
+    # c_max = data["Capacity"].max()
+    # Point_2 = bulkdata[bulkdata.Capacity == c_max]
+    print(Point)
+    energy_density = data.pivot('Porosity [-]','Thickness (μm)', 'En_Den')
+    power_density = data.pivot('Porosity [-]', 'Thickness (μm)', "Pow Den")
+    Delta = data.pivot('Porosity [-]', 'Thickness (μm)', "Delta")
+    # capacity = data.pivot('Porosity [-]', 'Thickness (μm)', "Capacity")
+    GHG= data.pivot('Porosity [-]', 'Thickness (μm)', "GHG Emissions [kg CO2 kWh-2]")
+    sns.heatmap(energy_density, ax=axes[a,b]).set_title(current + ': Max Energy Density =' + str(round(ed_max)) + ' Wh kg⁻¹') #, vmin=ed_min, vmax=ed_max
+    sns.heatmap(power_density, ax=axes2[a,b]).set_title(current + ': Max Power Density =' + str(round(pd_max,1)) + ' W kg⁻¹') #, vmin=pd_min, vmax=pd_max
+    sns.heatmap(Delta, ax=axes[1,b], cmap="seismic", vmin=1*d2_max, vmax=-d2_max).set_title(current ) #, vmin=-1*d_max, vmax=d_max  vmin=1*d2_max, vmax=-d2_max
+    sns.heatmap(GHG, ax=axes3[a,b], cmap="crest").set_title(current + ': Min GHG =' + str(round(g_min)) + ' kg CO2 kWh⁻¹') #, vmin=pd_min, vmax=pd_max
+
+
+
+
 
 axes[0,0].set(xticklabels=[])
 axes[0,1].set(xticklabels=[])
@@ -106,6 +225,7 @@ axes[0,1].set(ylabel=None)
 axes[1,1].set(ylabel=None)
 
 # fig.savefig('heatmap_ed'+ '.png', format='png', dpi=900, bbox_inches = "tight")
+fig.savefig('graded_pc'+ '.png', format='png', dpi=300, bbox_inches = "tight")
 axes2[0,0].set(xticklabels=[])
 axes2[0,1].set(xticklabels=[])
 axes2[0,0].set(yticklabels=pointer)
@@ -143,7 +263,7 @@ axes3[1,1].set(ylabel=None)
 # fig3.savefig('heatmap_ghg'+ '.png', format='png', dpi=900, bbox_inches = "tight")
 # axes4[0,0].set(xticklabels=[])
 # axes4[0,1].set(xticklabels=[])
-# axes4[0,0].set(yticklabels=pointer)
+axes4[0,0].set(yticklabels=pointer)
 # axes4[1,0].set(yticklabels=pointer)
 # axes4[0,0].set(xlabel=None)
 # axes4[0,1].set(xlabel=None)
@@ -151,10 +271,10 @@ axes3[1,1].set(ylabel=None)
 # axes4[0,1].tick_params(bottom=False)
 
 # axes4[1,1].set(yticklabels=[])
-# axes4[0,1].set(yticklabels=[])
-# axes4[0,1].tick_params(left=False)
+axes4[0,1].set(yticklabels=[])
+axes4[0,1].tick_params(left=False)
 # axes4[1,1].tick_params(left=False)
-# axes4[0,1].set(ylabel=None)
+axes4[0,1].set(ylabel=None)
 # axes4[1,1].set(ylabel=None)
 
 
--- batteries/Figure_summary.py
+++ batteries/Figure_summary.py
@@ -17,14 +17,15 @@ keys = Sensitivity.keys()
 plt.figure(figsize=(20, 16))
 for n, key in enumerate(keys):
     ax1 = plt.subplot(3, 2, n + 1)
-    ax1.set_ylim(Sensitivity[key][columns[1]].min()-0.05,Sensitivity[key][columns[1]].max()+0.05)
     columns = Sensitivity[key].columns
+    ax1.set_ylim(Sensitivity[key][columns[1]].min()-0.05,Sensitivity[key][columns[1]].max()+0.05)
+
     if Sensitivity[key][columns[0]].min() <0.0003:
         ax1.set_xscale('log')
-    ax1.scatter(Sensitivity[key][columns[0]], Sensitivity[key][columns[1]], c='#648FFF', label =columns[1], s =180, marker = 's')
+    ax1.plot(Sensitivity[key][columns[0]], Sensitivity[key][columns[1]], c='#648FFF', linestyle='-.', linewidth=5,label =columns[1], markersize =20, marker = 's')
     ax2 = ax1.twinx()
-    ax2.scatter(Sensitivity[key][columns[0]], Sensitivity[key][columns[2]], marker = 'o', c='#DC267F', label =columns[2], s =180)
-    ax2.scatter(Sensitivity[key][columns[0]], Sensitivity[key][columns[3]], marker = 'v', c='#FE6100', label =columns[3], s =180)
+    ax2.plot(Sensitivity[key][columns[0]], Sensitivity[key][columns[2]], marker = 'o',linestyle='--', linewidth=5, c='#DC267F', label =columns[2], markersize =20)
+    ax2.plot(Sensitivity[key][columns[0]], Sensitivity[key][columns[3]], marker = 'v', c='#FE6100',linestyle='-.', linewidth=5, label =columns[3], markersize =20)
     ax1.set_ylabel("Porosity [-]")
     ax2.set_ylabel("Δ%")
     ax1.set_xlabel(columns[0])
@@ -33,14 +34,14 @@ for n, key in enumerate(keys):
         ax1.set_xlim(1.E-10,5E-8)
         # ax2.set_ylim(-200,900)
     if n==3:
-        ax2.set_ylim(-100,400)
+        ax2.set_ylim(-100,350)
 
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 plt.subplots_adjust(wspace=0.40, hspace=0.25)
-ax1.legend(h1+h2, l1+l2, ncol = 3, bbox_to_anchor =(0.3,2.5))
+ax1.legend(h1+h2, l1+l2, ncol = 3, bbox_to_anchor =(0.4,2.5))
 
-plt.savefig('Results'+ '.png', format='png', dpi=900, bbox_inches = "tight")
+plt.savefig('Results'+ '.png', format='png', dpi=300, bbox_inches = "tight")
 
 # %%
 
